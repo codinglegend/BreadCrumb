28177982e9e48d9cc0e2a6330898e674a8f15ea2
Alain K <alain.kahwaji@gmail.com>
2015-06-06 18:12:47 -0700

    Remove CoreData errors

---
+++ CoreDataStack.h
@@ -0,0 +1,21 @@
+//
+//  CoreDataStack.h
+//  RoadtripTrackerCoreData
+//
+//  Created by Alain  on 2015-06-06.
+//  Copyright (c) 2015 Alain . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@interface CoreDataStack : NSObject
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
